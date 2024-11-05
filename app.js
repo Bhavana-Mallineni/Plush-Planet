42c30b971f00f07985ac61490d628f3834bcdce8
Bhavana-Mallineni <mallinenibhavana123@gmail.com>
2024-11-05 15:38:09 +0530

    added seperation of concerns in models and created owner model

--- app.js
+++ app.js
@@ -6,7 +6,21 @@ const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
+const db = require("./config/mongoose-connection");
+
+const ownersRouter = require('./routes/ownersRouter.js');
+const productsRouter = require('./routes/productsRouter.js');
+const usersRouter = require('./routes/usersRouter.js');
+
 app.use(express.json());
-app.use(express.urlencoded, {extended : true});
+app.use(express.urlencoded({extended : true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
+app.listen(3000);
+
+
